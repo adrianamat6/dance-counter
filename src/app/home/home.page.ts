9d5792e70453b24415fd78784d78e3bb003074bc
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-09-05 20:59:07 +0200

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonIcon,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { micOutline, flagOutline, refreshOutline } from 'ionicons/icons';
+import { BeatCounterService, CounterMode } from '../services/beat-counter.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel,
+    IonIcon,
+    IonButton,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  protected readonly counter = inject(BeatCounterService);
+  protected mode: CounterMode = 'manual';
+
+  constructor() {
+    addIcons({ micOutline, flagOutline, refreshOutline });
+  }
+
+  protected async setMode(mode: CounterMode): Promise<void> {
+    if (mode === this.mode) return;
+    this.mode = mode;
+
+    if (mode === 'mic') {
+      await this.counter.startListening();
+    } else {
+      this.counter.stopListening();
+      this.counter.reset();
+    }
+  }
+
+  protected onTap(): void {
+    if (this.mode === 'manual') this.counter.tap();
+  }
+
+  protected setBeatsPerBar(value: number): void {
+    this.counter.setBeatsPerBar(value);
+  }
+
+  ngOnDestroy(): void {
+    this.counter.stopListening();
+  }
 }
